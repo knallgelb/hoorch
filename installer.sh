2b8169169ba57caa8c1dc9506519a0a1cc0bb085
Rainer Amler <rainer@amler.at>
2024-09-30 14:23:50 +0200

    a lot of changes to install.sh

--- installer.sh
+++ installer.sh
@@ -1,18 +1,23 @@
 #!/bin/sh
 # installer.sh will install the necessary packages and config files for HOORCH
+# Enter sudo-mode: sudo -i
 
 # Install packages
 echo "apt updating system. this may take some time..."
-apt update -y
-apt upgrade -y
+sudo apt update -y
+sudo apt upgrade -y
 
 echo "installing packages"
-apt install -y python3-pip
-apt install -y sox
-apt install -y libsox-fmt-mp3
-apt install -y espeak
-apt install -y libsdl2-mixer-2.0-0
-apt install -y git
+sudo apt install -y python3-full
+sudo apt install -y python3-pip
+sudo apt install -y sox
+sudo apt install -y libsox-fmt-mp3
+sudo apt install -y espeak
+sudo apt install -y libsdl2-mixer-2.0-0
+sudo apt install -y git
+
+python3 -m venv venv
+source venv/bin/activate
 
 pip3 install --upgrade setuptools
 pip3 install flask werkzeug ndeflib RPI.GPIO adafruit-circuitpython-pn532 board pygame rpi_ws281x adafruit-circuitpython-neopixel adafruit-circuitpython-debouncer
@@ -20,17 +25,76 @@ python3 -m pip install --force-reinstall adafruit-blinka
 pip3 install --upgrade adafruit-python-shell
 
 #enable SPI
-sed -i "s/#dtparam=spi=on/dtparam=spi=on/g" "/boot/config.txt"
+sudo sed -i "s/#dtparam=spi=on/dtparam=spi=on/g" "/boot/firmware/config.txt"
 
 # i2s microphone
-wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/i2smic.py
-python3 i2smic.py
+#wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/i2smic.py
+#sudo python3 i2smic.py
+sudo apt install dkms raspberrypi-kernel-headers -y
+wget https://github.com/opencardev/snd-i2s_rpi/releases/download/v0.0.2/snd-i2s-rpi-dkms_0.0.2_all.deb
+sudo dpkg -i snd-i2s-rpi-dkms_0.0.2_all.deb
+sudo modprobe snd-i2s_rpi rpi_platform_generation=0
 
 # i2s microphone - add volume control
-bash -c 'cat .asoundrc >> /etc/asound.conf'
+# bash -c 'cat .asoundrc >> /etc/asound.conf'
 
 # i2s amplifier
-curl -sS https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/i2samp.sh | bash
+# curl -sS https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/i2samp.sh | bash
+sudo tee /etc/asound.conf > /dev/null << EOF
+pcm.speakerbonnet {
+   type hw card 0
+}
+
+pcm.dmixer {
+   type dmix
+   ipc_key 1024
+   ipc_perm 0666
+   slave {
+     pcm "speakerbonnet"
+     period_time 0
+     period_size 1024
+     buffer_size 8192
+     rate 44100
+     channels 2
+   }
+}
+
+ctl.dmixer {
+   type hw card 0
+}
+
+pcm.softvol {
+   type softvol
+   slave.pcm "dmixer"
+   control.name "PCM"
+   control.card 0
+}
+
+ctl.softvol {
+   type hw card 0
+}
+
+pcm.!default {
+   type plug
+   slave.pcm "softvol"
+}
+EOF
+
+sudo sed -i "s/#dtparam=spi=on/dtparam=spi=on/g" "/boot/firmware/config.txt"
+
+# Setup MAX98357
+sudo sed -i 's/^\(dtparam=audio=on\)/#\1/' "/boot/firmware/config.txt"
+
+# Überprüfen, ob dtoverlay=hifiberry-dac bereits vorhanden ist, sonst hinzufügen
+if ! grep -Fxq "dtoverlay=hifiberry-dac" /boot/firmware/config.txt; then
+  echo "dtoverlay=hifiberry-dac" | sudo tee -a /boot/firmware/config.txt
+fi
+
+# Überprüfen, ob dtoverlay=i2s-mmap bereits vorhanden ist, sonst hinzufügen
+if ! grep -Fxq "dtoverlay=i2s-mmap" /boot/firmware/config.txt; then
+  echo "dtoverlay=i2s-mmap" | sudo tee -a /boot/firmware/config.txt
+fi
+
 
 #disable hdmi (enable: -p) - to safe power - i need hdmi connection 
 #/opt/vc/bin/tvservice -o
@@ -44,11 +108,17 @@ curl -sS https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scrip
 #sh -c "echo 'dtparam=pwr_led_activelow=off' >> /boot/config.txt"
 
 #disable bluetooth - to safe power
-sh -c "echo 'dtoverlay=pi3-disable-bt' >> /boot/config.txt"
-systemctl disable bluetooth.service
+# Überprüfen, ob dtoverlay=i2s-mmap bereits vorhanden ist, sonst hinzufügen
+if ! grep -Fxq "dtoverlay=pi3-disable-bt" /boot/firmware/config.txt; then
+  echo "dtoverlay=pi3-disable-bt" | sudo tee -a /boot/firmware/config.txt
+fi
+
+sudo systemctl disable bluetooth.service
 
 # Disable the rainbow splash screen
-sh -c "echo 'disable_splash=1' >> /boot/config.txt"
+if ! grep -Fxq "disable_splash=1" /boot/firmware/config.txt; then
+  echo "disable_splash=1" | sudo tee -a /boot/firmware/config.txt
+fi
 
 # Set the bootloader delay to 0 seconds. The default is 1s if not specified.
 #sh -c "echo 'boot_delay=0' >> /boot/config.txt"
@@ -70,49 +140,46 @@ chmod +x /lib/systemd/system-shutdown/gpio-shutoff.sh
 
 
 #install log2ram
-echo "deb http://packages.azlux.fr/debian/ buster main" | sudo tee /etc/apt/sources.list.d/azlux.list
-wget -qO - https://azlux.fr/repo.gpg.key | sudo apt-key add -
-apt update
-apt install log2ram
+echo "deb [signed-by=/usr/share/keyrings/azlux-archive-keyring.gpg] http://packages.azlux.fr/debian/ bookworm main" | sudo tee /etc/apt/sources.list.d/azlux.list
+wget -O - https://azlux.fr/repo.gpg.key | gpg --dearmor | sudo tee /usr/share/keyrings/azlux-archive-keyring.gpg > /dev/null
+sudo apt update
+sudo apt install log2ram
 
 #disable swapping
-systemctl disable dphys-swapfile.service
+sudo systemctl disable dphys-swapfile.service
 
 #change SIZE=40M to SIZE=100M /etc/log2ram.conf
-sed -i 's/SIZE=40M/SIZE=100M/g' /etc/log2ram.conf
+sudo sed -i 's/SIZE=40M/SIZE=100M/g' /etc/log2ram.conf
 
 #start/restart networking service (NetworkManager)
-systemctl restart networking
+sudo systemctl restart networking
 
 #install comitup - wifi:
 #1 install package .deb
-#wget https://davesteele.github.io/comitup/latest/davesteele-comitup-apt-source_latest.deb
-#dpkg -i --force-all davesteele-comitup-apt-source_latest.deb
-#rm davesteele-comitup-apt-source_latest.deb
-apt update
+sudo apt update
 # apt install comitup comitup-watch -y
-apt install comitup -y
+sudo apt install comitup -y
 
 #2: Allow NetworkManager to manage the wifi interfaces by removing references to them from /etc/network/interfaces.
-mv /etc/network/interfaces /etc/network/interfaces.bak
+sudo mv /etc/network/interfaces /etc/network/interfaces.bak
 
 #3: Rename  /etc/wpa_supplicant/wpa_supplicant.conf.
-mv /etc/wpa_supplicant/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.confbak
+sudo mv /etc/wpa_supplicant/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.confbak
 
 #4: The systemd.resolved service should be disabled and masked to avoid contention for providing DNS service.
-systemctl mask dnsmasq.service
-systemctl mask systemd-resolved.service
-systemctl mask dhcpd.service
-systemctl mask dhcpcd.service
-systemctl mask wpa-supplicant.service
-systemctl enable NetworkManager.service
+sudo systemctl mask dnsmasq.service
+sudo systemctl mask systemd-resolved.service
+sudo systemctl mask dhcpd.service
+sudo systemctl mask dhcpcd.service
+sudo systemctl mask wpa-supplicant.service
+sudo systemctl enable NetworkManager.service
 
 #5: #rename the comitup-wifi-name to hoorch-<nn> - https://davesteele.github.io/comitup/man/comitup-conf.5.html
-sed -i "s/# ap_name: comitup-<nnn>/ap_name: hoorch-<nnn>/g" "/etc/comitup.conf"
+sudo sed -i "s/# ap_name: comitup-<nnn>/ap_name: hoorch-<nnn>/g" "/etc/comitup.conf"
 
 #6 #add web_service to be started in CONNECTED stage of compitup
 #web_service: This defines a user web service to be controlled by comitup. This service will be disabled in the HOTSPOT state in preference of the comitup web service, and will be enabled in the CONNECTED state. This should be the name of the systemd web service, such as apache2.service or nginx.service. This defaults to a null string, meaning no service is controlled
-sed -i "s/# web_service:/web_service: hoorch-webserver.service/g" "/etc/comitup.conf"
+sudo sed -i "s/# web_service:/web_service: hoorch-webserver.service/g" "/etc/comitup.conf"
 
 echo "Installation complete, rebooting now"
-reboot
+sudo reboot
