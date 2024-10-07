05e478b4967ab0bb38ed400501d3818940bc6e47
Rainer Amler <rainer@amler.at>
2024-10-07 14:09:23 +0200

    update installer

--- installer.sh
+++ installer.sh
@@ -16,31 +16,30 @@ sudo apt install -y espeak
 sudo apt install -y libsdl2-mixer-2.0-0
 sudo apt install -y git
 sudo apt install -y vim
+sudo apt install -y python3-venv
 
+# Set up the virtual environment
+echo "Setting up Python virtual environment"
 python3 -m venv venv
-source venv/bin/activate
+. venv/bin/activate
 
-pip3 install --upgrade setuptools
-pip3 install flask werkzeug ndeflib RPI.GPIO adafruit-circuitpython-pn532 board pygame rpi_ws281x adafruit-circuitpython-neopixel adafruit-circuitpython-debouncer
-python3 -m pip install --force-reinstall adafruit-blinka
-pip3 install --upgrade adafruit-python-shell
+# Install Python dependencies inside the virtual environment
+echo "Installing Python dependencies"
+pip install --upgrade pip setuptools
+pip install flask werkzeug ndeflib RPI.GPIO adafruit-circuitpython-pn532 board pygame rpi_ws281x adafruit-circuitpython-neopixel adafruit-circuitpython-debouncer
+pip install --force-reinstall adafruit-blinka
+pip install --upgrade adafruit-python-shell
 
-#enable SPI
+# Enable SPI
 sudo sed -i "s/#dtparam=spi=on/dtparam=spi=on/g" "/boot/firmware/config.txt"
 
 # i2s microphone
-#wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/i2smic.py
-#sudo python3 i2smic.py
 sudo apt install dkms raspberrypi-kernel-headers -y
 wget https://github.com/opencardev/snd-i2s_rpi/releases/download/v0.0.2/snd-i2s-rpi-dkms_0.0.2_all.deb
 sudo dpkg -i snd-i2s-rpi-dkms_0.0.2_all.deb
 sudo modprobe snd-i2s_rpi rpi_platform_generation=0
 
-# i2s microphone - add volume control
-# bash -c 'cat .asoundrc >> /etc/asound.conf'
-
-# i2s amplifier
-# curl -sS https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/i2samp.sh | bash
+# i2s amplifier configuration
 sudo tee /etc/asound.conf > /dev/null << EOF
 pcm.speakerbonnet {
    type hw card 0
@@ -81,35 +80,19 @@ pcm.!default {
 }
 EOF
 
-sudo sed -i "s/#dtparam=spi=on/dtparam=spi=on/g" "/boot/firmware/config.txt"
-
 # Setup MAX98357
 sudo sed -i 's/^\(dtparam=audio=on\)/#\1/' "/boot/firmware/config.txt"
 
-# Überprüfen, ob dtoverlay=hifiberry-dac bereits vorhanden ist, sonst hinzufügen
+# Add overlays if not already present
 if ! grep -Fxq "dtoverlay=hifiberry-dac" /boot/firmware/config.txt; then
   echo "dtoverlay=hifiberry-dac" | sudo tee -a /boot/firmware/config.txt
 fi
 
-# Überprüfen, ob dtoverlay=i2s-mmap bereits vorhanden ist, sonst hinzufügen
 if ! grep -Fxq "dtoverlay=i2s-mmap" /boot/firmware/config.txt; then
   echo "dtoverlay=i2s-mmap" | sudo tee -a /boot/firmware/config.txt
 fi
 
-
-#disable hdmi (enable: -p) - to safe power - i need hdmi connection 
-#/opt/vc/bin/tvservice -o
-
-# Disable the ACT LED.
-#sh -c "echo 'dtparam=act_led_trigger=none' >> /boot/config.txt"
-#sh -c "echo 'dtparam=act_led_activelow=off' >> /boot/config.txt"
-
-# Disable the PWR LED.
-#sh -c "echo 'dtparam=pwr_led_trigger=none' >> /boot/config.txt"
-#sh -c "echo 'dtparam=pwr_led_activelow=off' >> /boot/config.txt"
-
-#disable bluetooth - to safe power
-# Überprüfen, ob dtoverlay=i2s-mmap bereits vorhanden ist, sonst hinzufügen
+# Disable Bluetooth to save power
 if ! grep -Fxq "dtoverlay=pi3-disable-bt" /boot/firmware/config.txt; then
   echo "dtoverlay=pi3-disable-bt" | sudo tee -a /boot/firmware/config.txt
 fi
@@ -121,53 +104,39 @@ if ! grep -Fxq "disable_splash=1" /boot/firmware/config.txt; then
   echo "disable_splash=1" | sudo tee -a /boot/firmware/config.txt
 fi
 
-# Set the bootloader delay to 0 seconds. The default is 1s if not specified.
-#sh -c "echo 'boot_delay=0' >> /boot/config.txt"
-
-echo "copying HOORCH files"
-
-#copy service-files to /etc/systemd/system
+# Copy HOORCH files
+echo "Copying HOORCH files"
 sudo cp *.service /etc/systemd/system
-
-#copy gpio shutoff script for OnOff Shim and make it executeable
 sudo cp gpio-shutoff.sh /lib/systemd/system-shutdown/
 sudo chmod +x /lib/systemd/system-shutdown/gpio-shutoff.sh
 
-#enable and start the services
+# Enable and start HOORCH services
 sudo systemctl enable hoorch*.service
 sudo systemctl start hoorch*.service
 
-#will be started manually by user, see installation manual
-
-
-#install log2ram
+# Install log2ram
+echo "Installing log2ram"
 echo "deb [signed-by=/usr/share/keyrings/azlux-archive-keyring.gpg] http://packages.azlux.fr/debian/ bookworm main" | sudo tee /etc/apt/sources.list.d/azlux.list
 wget -O - https://azlux.fr/repo.gpg.key | gpg --dearmor | sudo tee /usr/share/keyrings/azlux-archive-keyring.gpg > /dev/null
 sudo apt update
 sudo apt install log2ram
 
-#disable swapping
+# Disable swapping
 sudo systemctl disable dphys-swapfile.service
 
-#change SIZE=40M to SIZE=100M /etc/log2ram.conf
+# Update log2ram configuration
 sudo sed -i 's/SIZE=40M/SIZE=100M/g' /etc/log2ram.conf
 
-#start/restart networking service (NetworkManager)
+# Restart networking service
 sudo systemctl restart networking
 
-#install comitup - wifi:
-#1 install package .deb
+# Install comitup - wifi setup
 sudo apt update
-# apt install comitup comitup-watch -y
 sudo apt install comitup -y
-
-#2: Allow NetworkManager to manage the wifi interfaces by removing references to them from /etc/network/interfaces.
 sudo mv /etc/network/interfaces /etc/network/interfaces.bak
-
-#3: Rename  /etc/wpa_supplicant/wpa_supplicant.conf.
 sudo mv /etc/wpa_supplicant/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.confbak
 
-#4: The systemd.resolved service should be disabled and masked to avoid contention for providing DNS service.
+# Disable unnecessary services
 sudo systemctl mask dnsmasq.service
 sudo systemctl mask systemd-resolved.service
 sudo systemctl mask dhcpd.service
@@ -175,11 +144,8 @@ sudo systemctl mask dhcpcd.service
 sudo systemctl mask wpa-supplicant.service
 sudo systemctl enable NetworkManager.service
 
-#5: #rename the comitup-wifi-name to hoorch-<nn> - https://davesteele.github.io/comitup/man/comitup-conf.5.html
+# Update comitup configuration
 sudo sed -i "s/# ap_name: comitup-<nnn>/ap_name: hoorch-<nnn>/g" "/etc/comitup.conf"
-
-#6 #add web_service to be started in CONNECTED stage of compitup
-#web_service: This defines a user web service to be controlled by comitup. This service will be disabled in the HOTSPOT state in preference of the comitup web service, and will be enabled in the CONNECTED state. This should be the name of the systemd web service, such as apache2.service or nginx.service. This defaults to a null string, meaning no service is controlled
 sudo sed -i "s/# web_service:/web_service: hoorch-webserver.service/g" "/etc/comitup.conf"
 
 echo "Installation complete, rebooting now"
