8e4730c5c24402a9c394360204fa2f465ccc4a78
Rainer Amler <rainer@amler.at>
2024-12-26 18:00:50 +0100

    only announce rfidtags

--- games/game_utils.py
+++ games/game_utils.py
@@ -3,10 +3,13 @@ import audio
 import rfidreaders
 import leds
 import file_lib
+from models import RFIDTag
+
 
 def check_end_tag():
     """Return True if the ENDE tag is detected, else False."""
     return file_lib.check_tag_attribute(rfidreaders.tags, "ENDE", "name")
+
 
 def announce(msg_id, path='TTS'):
     """Play a message by its ID from the given path and check for ENDE tag."""
@@ -14,23 +17,29 @@ def announce(msg_id, path='TTS'):
     if check_end_tag():
         raise SystemExit
 
+
 def announce_file(msg_id, path='TTS'):
     """Play a message by its ID from the given path and check for ENDE tag."""
     audio.play_file(path, msg_id)
     if check_end_tag():
         raise SystemExit
 
+
 def announce_score(score_players: dict):
     """Play a message by its ID from the given path and check for ENDE tag."""
     audio.play_full("TTS", 80)
     for player, score in score_players.items():
+        if not isinstance(player,RFIDTag):
+            continue
         audio.espeaker(f"{player.name} hat {score} richtige Antworten.")
+
 
 def wait_for_figure_placement(fields):
     """Ask players to place their figures on specified fields and wait."""
     leds.switch_on_with_color(fields)
     audio.play_file("sounds", "waiting.mp3")
     time.sleep(6)
+
 
 def filter_players_on_fields(players, valid_fields, defined_figures):
     """Set players to None if they are not on the valid fields or not defined figures."""
@@ -41,6 +50,7 @@ def filter_players_on_fields(players, valid_fields, defined_figures):
             players[i] = None
     return players
 
+
 def blink_led(field_index, times=10, on_time=0.5, off_time=0.5):
     """Blink LED at field_index."""
     for _ in range(times):
@@ -48,6 +58,7 @@ def blink_led(field_index, times=10, on_time=0.5, off_time=0.5):
         time.sleep(on_time)
         leds.switch_on_with_color(field_index, (0, 0, 0))
         time.sleep(off_time)
+
 
 def get_solution_from_tags(i, players):
     """Calculate the solution from the tens and units tags."""
