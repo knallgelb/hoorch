ce0f42d34593f2ed0c636190a4d3310f8c98f357
Rainer Amler <rainer@amler.at>
2024-12-26 17:15:06 +0100

    zeit ist ein Hund

--- games/game_zahlen.py
+++ games/game_zahlen.py
@@ -6,7 +6,7 @@ import audio
 import rfidreaders
 
 import random
-from time import sleep
+import time
 from typing import List
 
 
@@ -14,7 +14,7 @@ def start():
     defined_figures = file_lib.figures_db
     audio.espeaker("Spiel Zahlen legen")
     audio.espeaker("Setze die Spielfiguren auf das Spielfeld. Mögliche Felder sind 1, 3, 5")
-    sleep(3.0)
+    time.sleep(3.0)
 
     rfid_position = [1, 3, 5]
 
@@ -42,24 +42,31 @@ def player_action(
         file_lib,
         rfid_position: List[int]
 ) -> bool:
-
     expected_value = random.choice(list(file_lib.animal_numbers_db.values()))
-
     game_utils.announce_score(expected_value.number)
 
-    sleep(3.0)
+    # Wir warten bis zu 3 Sekunden, prüfen aber zwischendurch immer wieder
+    total_wait_seconds = 3.0
+    start_time = time.time()
 
-    relevant_tags = [
-        rfidreaders.tags[pos]
-        for pos in rfid_position
-        if pos < len(rfidreaders.tags)
-    ]
+    while time.time() - start_time < total_wait_seconds:
+        relevant_tags = [
+            rfidreaders.tags[pos]
+            for pos in rfid_position
+            if pos < len(rfidreaders.tags)
+        ]
+        if any(
+            tag == expected_value
+            for tag in relevant_tags
+            if tag.number == expected_value.number
+        ):
+            return True
 
-    return any(
-        tag == expected_value
-        for tag in relevant_tags
-        if tag.number == expected_value.number
-    )
+        # Warten wir kurz und prüfen dann wieder
+        time.sleep(0.3)
+
+    # Wenn nach 3 Sekunden keine Übereinstimmung gefunden wurde, False
+    return False
 
 
 if __name__ == "__main__":
