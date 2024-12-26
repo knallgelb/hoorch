859332205ab25928a4f337b02ebc75932db1e106
Rainer Amler <rainer@amler.at>
2024-12-26 16:18:21 +0100

    add hashing to RFIDTAG

--- models.py
+++ models.py
@@ -1,8 +1,13 @@
 from dataclasses import dataclass
 
-@dataclass(frozen=True)
+
+# @dataclass(frozen=True)
+@dataclass
 class RFIDTag:
     rfid_tag: str
     name: str
     rfid_type: str
     number: int | None = None
+
+    def __hash__(self):
+        return hash((self.rfid_tag, self.name, self.rfid_type, self.number))
