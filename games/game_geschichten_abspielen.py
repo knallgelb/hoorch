9d533fd6efd5c0e4ca2ba2b0665e5a74add8a672
Rainer Amler <rainer@amler.at>
2025-03-10 10:23:09 +0100

    removed code

--- games/game_geschichten_abspielen.py
+++ games/game_geschichten_abspielen.py
@@ -24,7 +24,7 @@ def start():
     audio.play_full("TTS", 5)  # Stelle deine Figur auf eines der Spielfelder
 
     audio.play_file("sounds", "waiting.mp3")  # play wait sound
-    leds.rotate_one_round(1.11)
+    leds.rotate_one_round(0.55)
 
     if file_lib.check_tag_attribute(rfidreaders.tags, "ENDE", "name"):
         return
@@ -46,14 +46,6 @@ def start():
     if file_lib.check_tag_attribute(rfidreaders.tags, "ENDE", "name"):
         return
 
-    # remove figures without a recorded story from list
-    for i, figure_id in enumerate(players):
-        if figure_id is not None:
-            file_path = base_path / pathlib.Path(f"{figure_id.rfid_tag}/{figure_id.rfid_tag}.mp3")
-            if file_path.exists():
-                continue
-            players[i] = None
-
     figure_count = sum(x is not None for x in players)
     if figure_count == 0:
         # Keine deiner Spielfiguren hat eine Geschichte gespeichert.
@@ -69,45 +61,20 @@ def start():
     if file_lib.check_tag_attribute(rfidreaders.tags, "ENDE", "name"):
         return
 
-    first_round = True
-    for i, figure_id in enumerate(players):
+    for i, player in enumerate(players):
         leds.reset()
-        if figure_id is not None:
-            leds.switch_on_with_color(i, (0, 255, 0))
+        leds.switch_on_with_color(i, (0, 255, 0))
 
-            if first_round:  # at start
-                if figure_count > 1:
-                    # Es beginnt die Spielfigur auf Spielfeld x
-                    audio.play_full("TTS", 12 + i)
-                first_round = False
-                # Ich Spiele dir jetzt deine Geschichte vor, wenn du stoppen willst nimm deine Spielfigur vom Spielfeld
-                audio.play_full("TTS", 61)
-            else:
-                # Die nächste Spielfigur steht auf Spielfeld x
-                audio.play_full("TTS", 47 + i)
+        if file_lib.check_tag_attribute(rfidreaders.tags, "ENDE", "name"):
+            return
 
-            if file_lib.check_tag_attribute(rfidreaders.tags, "ENDE", "name"):
-                return
+        file_path = base_path / pathlib.Path(f"{player.rfid_tag}/{player.rfid_tag}.mp3")
+        if file_path.exists():
+            audio.play_story(player)
+        else:
+            # Du hast noch keine Geschichte aufgenommen!
+            audio.play_full("TTS", 62)
+            continue
 
-            # when figure folder exists and contains i.e. roboter.mp3
-            # if figure_id in recordings_list and figure_id+'.mp3' in os.listdir(figure_dir):
-            file_path = base_path / pathlib.Path(f"{figure_id.rfid_tag}/{figure_id.rfid_tag}.mp3")
-            if file_path.exists():
-                # play story
-                audio.play_story(figure_id)
-                # waitingtime = time.time() + float(subprocess.run(
-                #     ['soxi', '-D', './data/figures/' + figure_id + '/' + figure_id + '.mp3'], stdout=subprocess.PIPE,
-                #     check=False).stdout.decode('utf-8'))
-                # print(waitingtime)
-            else:
-                # Du hast noch keine Geschichte aufgenommen!
-                audio.play_full("TTS", 62)
-                continue
-
-            # while True:
-            #     if rfidreaders.tags[i] != figure_id or waitingtime < time.time() or "ENDE" in rfidreaders.tags:
-            #         audio.kill_sounds()
-            #         break
-
-            if file_lib.check_tag_attribute(rfidreaders.tags, "ENDE", "name"):
-                return
+        if file_lib.check_tag_attribute(rfidreaders.tags, "ENDE", "name"):
+            return
