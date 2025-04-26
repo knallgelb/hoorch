e59c8c1b929a075c15bcb15696d7b48e81ae8e97
Rainer Amler <rainer@amler.at>
2025-04-26 15:11:30 +0200

    added database actions to crud

--- crud.py
+++ crud.py
@@ -9,3 +9,16 @@ def add_game_entry(usage: Usage, db: Session = next(get_db())):
 
 def get_all_games(db: Session = next(get_db())):
     return db.exec(select(Usage)).all()
+
+def get_all_games_to_submit(db: Session = next(get_db())):
+    return db.exec(select(Usage).filter(Usage.is_transmitted == False)).all()
+
+def set_transmitted(usage: Usage, db: Session = next(get_db())):
+    u: Usage | None = db.exec(select(Usage).where(Usage.id == usage.id)).first()
+    if u is None:
+        return None
+    u.is_transmitted = True
+    db.add(u)
+    db.commit()
+    db.refresh(u)
+    return u
