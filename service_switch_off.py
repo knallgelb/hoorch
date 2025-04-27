119b57f51e02d5612fcf33fc65bab030f8e1056b
Rainer Amler <rainer@amler.at>
2025-04-27 18:13:19 +0200

    Refactor switch-off script into function with cleanup

--- service_switch_off.py
+++ service_switch_off.py
@@ -5,36 +5,46 @@
 
 import os
 import time
-import digitalio
+
 import board
+import digitalio
 from adafruit_debouncer import Debouncer
+
 import audio
 import leds
-import time
 
-print("starting switch off")
 
-off_btn = digitalio.DigitalInOut(board.D13)
-off_btn.direction = digitalio.Direction.INPUT
-off_btn.pull = digitalio.Pull.UP
+def wait_for_shutdown_button(
+    btn_pin=board.D13,
+    threshold_time=3,
+    poll_interval=0.1,
+):
+    print("starting switch off")
 
-off = Debouncer(off_btn, interval=0.05)
+    off_btn = digitalio.DigitalInOut(btn_pin)
+    off_btn.direction = digitalio.Direction.INPUT
+    off_btn.pull = digitalio.Pull.UP
 
-# push threshold (in seconds)
-threshold_time = 3
+    off = Debouncer(off_btn, interval=0.05)
 
-while True:
-    off.update()
+    try:
+        while True:
+            off.update()
 
-    if off.rose:
-        #if button is released, check if it was pressed for at least 3 seconds
-        if off.last_duration > threshold_time:
-            print("shutdown")
-            leds.blink = False
-            time.sleep(0.5)
-            leds.reset()
-            leds.switch_all_on_with_color((255,0,0))
-            audio.play_full("TTS", 3)  # Tschüss ich schalte mich jetzt aus
-            os.system("shutdown -P now")
-        
-    time.sleep(0.1)
+            # if button was released, check if it was pressed for at least threshold_time seconds
+            if off.rose:
+                if off.last_duration > threshold_time:
+                    print("shutdown")
+                    leds.blink = False
+                    time.sleep(0.5)
+                    leds.reset()
+                    leds.switch_all_on_with_color((255, 0, 0))
+                    audio.play_full("TTS", 3)  # "Tschüss ich schalte mich jetzt aus"
+                    os.system("shutdown -P now")
+            time.sleep(poll_interval)
+    finally:
+        off_btn.deinit()
+
+
+if __name__ == "__main__":
+    wait_for_shutdown_button()
