cf910b1dc04796db5ef685611e725b56e0754a8f
Rainer Amler <rainer@amler.at>
2025-05-19 11:29:22 +0200

    Convert RFIDTag from dataclass to SQLModel table

--- models.py
+++ models.py
@@ -9,15 +9,13 @@ from dotenv import load_dotenv
 dotenv_path = "/home/pi/hoorch/.env"
 load_dotenv(dotenv_path, override=True)
 
-@dataclass
-class RFIDTag:
+class RFIDTag(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
     rfid_tag: str
     name: str
     rfid_type: str
     number: int | None = None
 
-    def __hash__(self):
-        return hash((self.rfid_tag, self.name, self.rfid_type, self.number))
 
 class Usage(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
