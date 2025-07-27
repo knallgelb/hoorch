c80fff05f89aee055c2d71005694a03c2feabf2f
Rainer Amler <rainer@amler.at>
2025-07-27 10:16:54 +0200

    use new audiofiles for turn tags

--- games/game_utils.py
+++ games/game_utils.py
@@ -121,19 +121,8 @@ def play_rounds(players, num_rounds, player_action) -> dict:
             if player is not None:
                 audio.play_file(
                     "TTS",
-                    translator.translate(f"game.player_turn_start"),
+                    translator.translate(f"turn_tags.{player.name.lower()}"),
                 )
-                audio.play_file(
-                    "TTS",
-                    translator.translate(
-                        f"standard_tags.{player.name.lower()}"
-                    ),
-                )
-                audio.play_file(
-                    "TTS",
-                    translator.translate(f"game.player_turn_end"),
-                )
-                # audio.espeaker(f"Jetzt ist {player.name} an der Reihe.")
                 if player_action(player):
                     score_players[player] += 1
 
